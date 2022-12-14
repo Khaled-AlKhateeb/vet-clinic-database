c655824b4826d7f3b7b185b7bb85ffccba36260a
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2022-12-14 17:36:28 +0300

    fix gitflow

--- schema.sql
+++ schema.sql
@@ -1,11 +1,2 @@
 -- Table: public.animals
 
-CREATE TABLE animals 
-(
-	id integer PRIMARY KEY,
-	name varchar (50),
-	date_of_birth date,
-	escape_attempts integer,
-	neutered bool,
-	weight_kg decimal
-);
