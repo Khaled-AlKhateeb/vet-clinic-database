b0c5ca3ca96048fa795e2dbd3cc7c0e15220ede6
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2022-12-16 00:24:59 +0300

    Visits Table

--- queries.sql
+++ queries.sql
@@ -124,3 +124,77 @@ INNER JOIN owners
 ON owners.id = animals.owners_id
 GROUP BY owners.full_name
 ORDER BY count DESC LIMIT 1;
+
+-- [X] Who was the last animal seen by William Tatcher?
+SELECT animals.name AS Animal, vets.name AS Vet, date_of_visit
+FROM visits LEFT JOIN vets ON visits.vet_id = vets.id
+LEFT JOIN animals ON visits.animal_id = animals.id
+WHERE vets.name = 'William Tatcher'
+ORDER BY date_of_visit DESC LIMIT 1;
+
+-- [X] How many different animals did Stephanie Mendez see?
+SELECT vets.name AS "Vet",COUNT(*)
+FROM visits LEFT JOIN vets ON visits.vet_id = vets.id
+LEFT JOIN animals ON visits.animal_id = animals.id
+WHERE vets.name = 'Stephanie Mendez' GROUP BY vets.name;
+
+-- [X] List all vets and their specialties, including vets with no specialties.
+SELECT * FROM vets
+LEFT JOIN specializations
+ON vets.id = specializations.vet_id
+LEFT JOIN species
+ON specializations.species_id = species.id;
+
+-- [X] List all animals that visited Stephanie Mendez between April 1st and August 30th, 2020.
+SELECT animals.name AS "Animals", vets.name AS "Vet"
+FROM visits LEFT JOIN animals
+ON visits.animal_id = animals.id
+LEFT JOIN vets ON visits.vet_id = vets.id
+WHERE vets.name = 'Stephanie Mendez'
+AND date_of_visit BETWEEN '2020-04-01'
+AND '2020-08-30' GROUP BY animals.name, vets.name;
+
+-- [X] What animal has the most visits to vets?
+SELECT animals.name AS "Animal", COUNT(*)
+FROM visits LEFT JOIN animals
+ON visits.animal_id = animals.id
+GROUP BY animals.name ORDER BY COUNT(*) DESC LIMIT 1;
+
+-- [X] Who was Maisy Smith's first visit?
+SELECT animals.name FROM visits
+LEFT JOIN animals
+ON visits.animal_id = animals.id 
+LEFT JOIN vets ON visits.vet_id = vets.id
+WHERE vets.name = 'Maisy Smith'
+ORDER BY date_of_visit LIMIT 1;
+
+-- [X] Details for most recent visit: animal information, vet information, and date of visit.
+SELECT vets.name AS "Vet",animals.name AS "Animal", date_of_visit
+FROM visits LEFT JOIN animals
+ON visits.animal_id = animals.id 
+LEFT JOIN vets ON visits.vet_id = vets.id
+WHERE vets.name = 'Maisy Smith'
+ORDER BY date_of_visit LIMIT 1;
+
+-- [X] How many visits were with a vet that did not specialize in that animal's species?
+SELECT vets.name AS "Vet", COUNT(*)
+FROM visits LEFT JOIN vets
+ON visits.vet_id = vets.id
+LEFT JOIN specializations
+ON vets.id = specializations.vet_id
+LEFT JOIN species
+ON specializations.species_id = species.id
+WHERE specializations.species_id IS NULL
+OR specializations.species_id != species.id
+GROUP BY vets.name;
+
+-- [X] What specialty should Maisy Smith consider getting? Look for the species she gets the most.
+SELECT vets.name AS "Vet", species.name AS "Specie", COUNT(*)
+FROM visits LEFT JOIN vets
+ON visits.vet_id = vets.id
+LEFT JOIN animals
+ON visits.animal_id = animals.id
+LEFT JOIN species
+ON animals.species_id = species.id
+WHERE vets.name = 'Maisy Smith'
+GROUP BY vets.name, species.name LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE animals (
 );
 
 CREATE TABLE owners (
-id int NOT NULL GENERATED ALWAYS AS identity,
+id integer NOT NULL GENERATED ALWAYS AS identity,
 full_name varchar (50),
 age integer,
 PRIMARY KEY (id)
@@ -33,3 +33,36 @@ ALTER TABLE species ADD CONSTRAINT species_pk PRIMARY KEY (id);
 
 ALTER TABLE owners ADD CONSTRAINT owners_pk PRIMARY KEY (id);
 
+CREATE TABLE vets (
+id integer NOT NULL GENERATED ALWAYS AS identity,
+name varchar (50),
+age integer,
+date_of_graduation date,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE specializations (
+vet_id integer,
+species_id integer,
+PRIMARY KEY (vet_id, species_id),
+CONSTRAINT vets_pk
+FOREIGN KEY (vet_id)
+REFERENCES vets(id),
+CONSTRAINT species_pk
+FOREIGN KEY (species_id)
+REFERENCES species(id)
+);
+
+CREATE TABLE visits(
+id integer NOT NULL GENERATED ALWAYS AS identity,
+vet_id integer,
+animal_id integer,
+date_of_visit DATE,
+PRIMARY KEY (id),
+CONSTRAINT fk_vets
+FOREIGN KEY (vet_id)
+REFERENCES vets(id),
+CONSTRAINT fk_animals
+FOREIGN KEY (animal_id)
+REFERENCES animals(id)
+);
