20073d9244f7c97af2fa7c64ec80ae5575caa1a0
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2022-12-15 14:46:19 +0300

    join multi tables

--- queries.sql
+++ queries.sql
@@ -16,58 +16,112 @@ SELECT * FROM animals WHERE name = 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+-- [X] Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction
 BEGIN;
-
 SELECT * FROM animals;
-
 ROLLBACK;
+--
 
-SELECT * FROM animals;
-
+-- New transaction
 BEGIN;
-
+-- [X] Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-
+-- [X] Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
 UPDATE animals SET species = 'pokemon' WHERE species = '';
-
+-- [X] Commit the transaction.
+-- Verify that change was made and persists after commit
 COMMIT;
+--
 
 SELECT * FROM animals;
-
+-- [X] Delete all recordes
 BEGIN;
-
 DELETE FROM animals;
-
 ROLLBACK;
+--
 
-SELECT * FROM animals;
-
+-- Inside a transaction:
 BEGIN;
-
+-- [X] Delete all animals born after Jan 1st, 2022.
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-
+-- [X] Create a savepoint for the transaction.
 SAVEPOINT multitransactions;
-
+-- [X] Update all animals' weight to be their weight multiplied by -1.
 UPDATE animals SET weight_kg = weight_kg * -1;
-
+-- [X] Rollback to the savepoint
 ROLLBACK TO multitransactions;
-
+-- [X] Update all animals' weights that are negative to be their weight multiplied by -1.
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
-
+-- [X] Commit transaction
 COMMIT;
+--
 
-SELECT * FROM animals;
-
+-- [X] How many animals are there?
 SELECT count(name) FROM animals;
 
+-- [X] How many animals have never tried to escape?
 SELECT * FROM animals WHERE escape_attempts = 0;
 
+-- [X] What is the average weight of animals?
 SELECT avg(weight_kg) FROM animals;
 
+-- [X] Who escapes the most, neutered or not neutered animals?
 SELECT neutered AS isNeutered FROM animals WHERE escape_attempts = (SELECT max(escape_attempts) FROM animals);
 
+-- [X] What is the minimum and maximum weight of each type of animal?
 SELECT species, min(weight_kg) AS minimumWeight, max(weight_kg) AS maximumWeight FROM animals GROUP BY species;
 
+-- [X] What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT species, CAST(avg(escape_attempts) AS decimal(10)) AS averageEscape FROM animals
 WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
 GROUP BY species;
+
+-- Write queries (using JOIN) to answer the following questions:
+-- [X] What animals belong to Melody Pond?
+SELECT name AS AnimalName, full_name AS Owner
+FROM animals INNER JOIN owners ON animals.owners_id = owners.id
+WHERE owners.full_name = 'Melody Pond';
+
+-- [X] List of all animals that are pokemon (their type is Pokemon).
+SELECT animals.name AS AnimalName, species.name AS Species
+FROM animals
+INNER JOIN species
+ON animals.species_id = species.id
+WHERE species.name = 'Pokemon';
+
+-- [X] List all owners and their animals, remember to include those that don't own any animal.
+SELECT owners.full_name AS Owner, animals.name AS AnimalName
+FROM owners
+LEFT JOIN animals
+ON owners.id = animals.owners_id;
+
+-- [X] How many animals are there per species?
+SELECT COUNT(animals.name) AS AnimalsCount, species.name AS Species
+FROM animals
+INNER JOIN species
+ON animals.species_id = species.id
+GROUP BY species.name;
+
+-- [X] List all Digimon owned by Jennifer Orwell.
+SELECT owners.full_name AS Owner, animals.name As Animal, species.name AS Species
+FROM owners
+INNER JOIN animals
+ON owners.id = animals.owners_id
+INNER JOIN species
+ON species.id = animals.species_id
+WHERE species.name = 'Digimon' AND owners.full_name = 'Jennifer Orwell';
+
+-- [X] List all animals owned by Dean Winchester that haven't tried to escape.
+SELECT owners.full_name AS Owner, animals.name As Animal, animals.escape_attempts
+FROM owners
+INNER JOIN animals
+ON owners.id = animals.owners_id
+WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
+
+-- [X] Who owns the most animals?
+SELECT owners.full_name , COUNT(*)
+FROM owners
+INNER JOIN animals
+ON owners.id = animals.owners_id
+GROUP BY owners.full_name
+ORDER BY count DESC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -1,7 +1,6 @@
 -- Table: public.animals
 
-CREATE TABLE animals 
-(
+CREATE TABLE animals (
 	id integer PRIMARY KEY,
 	name varchar (50),
 	date_of_birth date,
@@ -10,3 +9,27 @@ CREATE TABLE animals
 	weight_kg decimal,
 	species varchar (100)
 );
+
+CREATE TABLE owners (
+id int NOT NULL GENERATED ALWAYS AS identity,
+full_name varchar (50),
+age integer,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+id int NOT NULL GENERATED ALWAYS AS identity,
+name varchar (50),
+PRIMARY KEY (id)
+);
+
+ALTER TABLE animals ADD id integer NOT NULL GENERATED ALWAYS AS identity;
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD species_id integer, ADD owners_id integer;
+
+ALTER TABLE species ADD CONSTRAINT species_pk PRIMARY KEY (id);
+
+ALTER TABLE owners ADD CONSTRAINT owners_pk PRIMARY KEY (id);
+
